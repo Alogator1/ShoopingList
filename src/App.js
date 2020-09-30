5689c9cfc8d9d9a0aadd1fa91ea1fde53a8a9749
unknown <vtelyachy@gmail.com>
2020-10-01 00:42:14 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import "./App.css";
 import Adding from './components/Adding';
 
 function App() {
-  // localStorage.setItem("List", JSON.stringify([
-  //   {apple: "ran out"},
-  //   {bread: "have"}
-  // ]))
   return (
     <div className="App">
       Shopping list
